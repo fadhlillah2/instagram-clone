2c2318aef37e0b778314190183cc49279dbfa221
fadhlillah2 <fadhlillah949699@gmail.com>
2021-04-14 07:43:57 +0700

    first commit

---
+++ server/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://azimuth:dlbiFoDj2dgvIkaY@cluster0.lmsdw.mongodb.net/test?retryWrites=true&w=majority",
+  JWT_SECRET: "qwewqesadqwe4asdasdasd",
+};
